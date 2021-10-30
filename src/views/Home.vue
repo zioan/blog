07ad45c342db161df61f8205a73692a8dc84e899
zioan <zaharia.ioan@gmail.com>
2021-10-30 15:03:48 +0200

    watch and watchEffect

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,19 +2,15 @@
   <div class="home">
     <h1>home</h1>
     <input type="text" v-model="search">
-    <p>search term - {{search}}</p>
-    <div v-for="name in names" :key="name">
-      <p>{{name}}</p>
-    </div>
-    <h2>Sorted names</h2>
     <div v-for="name in matchingNames" :key="name">
       <p>{{name}}</p>
     </div>
+    <button @click="handleClick">stop watching</button>
   </div>
 </template>
 
 <script>
-import { ref, computed } from 'vue'
+import { ref, computed, watch, watchEffect } from 'vue'
 
 export default {
   name: 'Home',
@@ -22,11 +18,32 @@ export default {
     const search = ref('')
     const names = ref(['ioan', 'zaharia', 'john', 'doe', 'will', 'smith'])
 
+// run every time the value change but not until value change
+    // watch(search, () => {
+    //   console.log("watch function ran")
+    // })
+   const stopWatch = watch(search, () => {
+      console.log("watch function ran")
+    })
+
+// run every time the value change AND on app first render
+    // watchEffect(() => {
+    //   console.log("watchEffect function ran", search.value)
+    // })
+   const stopEffect = watchEffect(() => {
+      console.log("watchEffect function ran", search.value)
+    })
+
     const matchingNames = computed(() => {
       return names.value.filter((name) => name.includes(search.value))
     })
+
+    const handleClick = () => {
+      stopWatch()
+      stopEffect()
+    }
     
-    return {names, search, matchingNames}
+    return {names, search, matchingNames, handleClick}
   }
 }
 </script>
