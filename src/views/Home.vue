c21070960230189ef05014fa3d1da85651c0747d
zioan <zaharia.ioan@gmail.com>
2021-10-30 14:50:00 +0200

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>home</h1>
+    <input type="text" v-model="search">
+    <p>search term - {{search}}</p>
+    <div v-for="name in names" :key="name">
+      <p>{{name}}</p>
+    </div>
+    <h2>Sorted names</h2>
+    <div v-for="name in matchingNames" :key="name">
+      <p>{{name}}</p>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref, computed } from 'vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  setup() {
+    const search = ref('')
+    const names = ref(['ioan', 'zaharia', 'john', 'doe', 'will', 'smith'])
+
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value))
+    })
+    
+    return {names, search, matchingNames}
   }
 }
 </script>
