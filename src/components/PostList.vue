d6b40b5e96931c0eab7baec3c0ed72e63d9ad81a
zioan <zaharia.ioan@gmail.com>
2021-10-31 09:00:08 +0100

    lifecicle hooks

---
+++ src/components/PostList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="post-list">
+    <h2>posts</h2>
+    <div v-for='post in posts' :key="post.id">
+      <SinglePost :post="post" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { onMounted, onUnmounted, onUpdated } from 'vue'
+import SinglePost from "./SinglePost.vue"
+export default {
+  props:['posts'],
+  setup(props){
+    onMounted(() => {
+      console.log('component mounted')
+    })
+    onUnmounted(() => {
+      console.log('component unmounted')
+    })
+    onUpdated(() => {
+      console.log('component updated')
+    })
+  },
+  components: {
+    SinglePost
+  },
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,49 +1,32 @@
 <template>
   <div class="home">
-    <h1>home</h1>
-    <input type="text" v-model="search">
-    <div v-for="name in matchingNames" :key="name">
-      <p>{{name}}</p>
-    </div>
-    <button @click="handleClick">stop watching</button>
+    <h1>Home</h1>
+    <PostList v-if="showPost" :posts="posts" />
+    <button @click="showPost = !showPost">toggle posts</button>
+    <button @click="posts.pop()">delete a post</button>
   </div>
 </template>
 
 <script>
-import { ref, computed, watch, watchEffect } from 'vue'
+import PostList from "../components/PostList.vue"
+import { ref } from 'vue'
 
 export default {
   name: 'Home',
+
+  components: {
+    PostList
+  },
+
   setup() {
-    const search = ref('')
-    const names = ref(['ioan', 'zaharia', 'john', 'doe', 'will', 'smith'])
+    const posts = ref([
+      {title: 'Welcome to the blog', body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias expedita est repellat optio quidem velit ipsam totam possimus unde, ducimus libero, magni fugit veritatis, laboriosam eligendi ipsum odit esse? Laborum autem repudiandae nesciunt aperiam sapiente voluptas officia iusto, aspernatur, modi voluptatibus dolore inventore assumenda itaque unde necessitatibus temporibus. Quae harum eveniet sapiente, vitae aperiam magnam laudantium quas in magni aliquid enim dignissimos, maiores dolorum dolorem nihil sit cupiditate nulla provident consectetur quia? Provident id temporibus placeat ullam? Ducimus, dignissimos? Magnam quidem voluptate rem, quae dignissimos fugiat architecto dolorum beatae sapiente, cumque delectus tempora consequatur in ab voluptatem, eius accusantium? Quo!', id: 1},
+      {title: 'Top 5 CSS tips', body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias expedita est repellat optio quidem velit ipsam totam possimus unde, ducimus libero, magni fugit veritatis, laboriosam eligendi ipsum odit esse? Laborum autem repudiandae nesciunt aperiam sapiente voluptas officia iusto, aspernatur, modi voluptatibus dolore inventore assumenda itaque unde necessitatibus temporibus. Quae harum eveniet sapiente, vitae aperiam magnam laudantium quas in magni aliquid enim dignissimos, maiores dolorum dolorem nihil sit cupiditate nulla provident consectetur quia? Provident id temporibus placeat ullam? Ducimus, dignissimos? Magnam quidem voluptate rem, quae dignissimos fugiat architecto dolorum beatae sapiente, cumque delectus tempora consequatur in ab voluptatem, eius accusantium? Quo!', id: 2}
+    ])
 
-// run every time the value change but not until value change
-    // watch(search, () => {
-    //   console.log("watch function ran")
-    // })
-   const stopWatch = watch(search, () => {
-      console.log("watch function ran")
-    })
-
-// run every time the value change AND on app first render
-    // watchEffect(() => {
-    //   console.log("watchEffect function ran", search.value)
-    // })
-   const stopEffect = watchEffect(() => {
-      console.log("watchEffect function ran", search.value)
-    })
-
-    const matchingNames = computed(() => {
-      return names.value.filter((name) => name.includes(search.value))
-    })
-
-    const handleClick = () => {
-      stopWatch()
-      stopEffect()
-    }
-    
-    return {names, search, matchingNames, handleClick}
+    const showPost = ref(true)
+   
+    return {posts, showPost}
   }
 }
 </script>
