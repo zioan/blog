88f6be9884280dd8894b98c149401dfc9bfab6b2
zioan <zaharia.ioan@gmail.com>
2021-10-31 13:39:05 +0100

    fetch

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,40 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <PostList v-if="showPost" :posts="posts" />
-    <button @click="showPost = !showPost">toggle posts</button>
-    <button @click="posts.pop()">delete a post</button>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+    <div v-else>Loading...</div>
   </div>
 </template>
 
 <script>
-import PostList from "../components/PostList.vue"
 import { ref } from 'vue'
 
+import PostList from '../components/PostList.vue'
 export default {
   name: 'Home',
-
-  components: {
-    PostList
+  components: { PostList },
+  setup() { 
+    const posts = ref([])
+    const error = ref(null)
+    const load = async () => {
+      try {
+        let data = await fetch('http://localhost:3000/posts')
+        if(!data.ok) {
+          throw Error('no available data')
+        }
+        posts.value = await data.json()
+        console.log(posts.value)
+      }
+      catch(err) {
+        error.value = err.message
+        console.log(error.value)
+      }
+    }
+    load()
+    
+    return { posts, error }
   },
-
-  setup() {
-    const posts = ref([
-      {title: 'Welcome to the blog', body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias expedita est repellat optio quidem velit ipsam totam possimus unde, ducimus libero, magni fugit veritatis, laboriosam eligendi ipsum odit esse? Laborum autem repudiandae nesciunt aperiam sapiente voluptas officia iusto, aspernatur, modi voluptatibus dolore inventore assumenda itaque unde necessitatibus temporibus. Quae harum eveniet sapiente, vitae aperiam magnam laudantium quas in magni aliquid enim dignissimos, maiores dolorum dolorem nihil sit cupiditate nulla provident consectetur quia? Provident id temporibus placeat ullam? Ducimus, dignissimos? Magnam quidem voluptate rem, quae dignissimos fugiat architecto dolorum beatae sapiente, cumque delectus tempora consequatur in ab voluptatem, eius accusantium? Quo!', id: 1},
-      {title: 'Top 5 CSS tips', body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias expedita est repellat optio quidem velit ipsam totam possimus unde, ducimus libero, magni fugit veritatis, laboriosam eligendi ipsum odit esse? Laborum autem repudiandae nesciunt aperiam sapiente voluptas officia iusto, aspernatur, modi voluptatibus dolore inventore assumenda itaque unde necessitatibus temporibus. Quae harum eveniet sapiente, vitae aperiam magnam laudantium quas in magni aliquid enim dignissimos, maiores dolorum dolorem nihil sit cupiditate nulla provident consectetur quia? Provident id temporibus placeat ullam? Ducimus, dignissimos? Magnam quidem voluptate rem, quae dignissimos fugiat architecto dolorum beatae sapiente, cumque delectus tempora consequatur in ab voluptatem, eius accusantium? Quo!', id: 2}
-    ])
-
-    const showPost = ref(true)
-   
-    return {posts, showPost}
-  }
 }
 </script>
